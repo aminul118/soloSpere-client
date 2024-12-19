87b7c1abc56bd8ecea9d3a234ea055f4a9f68ecb
Md Aminul Islam <mr.aminul118@gmail.com>
2024-12-19 23:32:38 +0600

    Date format in job card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
 
+import { format } from "date-fns";
 import { Link } from "react-router-dom";
 
 const JobCard = ({ job }) => {
-  // console.log(job);
+  console.log(job);
+  const formatDate = format(new Date(job.date.startDate), "dd/MM/yyyy");
 
   return (
     <Link
       to={`/job/${job._id}`}
-      className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all"
+      className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all "
     >
       <div className="flex items-center justify-between">
         <span className="text-xs font-light text-gray-800 ">
-          Deadline: 28/05/2024
+          Deadline: {formatDate}
         </span>
         <span className="px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full ">
           {job.category}
